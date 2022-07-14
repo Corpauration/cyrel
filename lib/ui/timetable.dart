e46532b2a5e78cbac1c7bbb67169561dc83bf7b3
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2022-07-15 00:45:19 +0200

    ass missing file

---
+++ lib/ui/timetable.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TimeTable extends StatefulWidget {
+  const TimeTable({Key? key}) : super(key: key);
+
+  @override
+  State<TimeTable> createState() => _TimeTableState();
+}
+
+class _TimeTableState extends State<TimeTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.amber,);
+  }
+}
