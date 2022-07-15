d10555b88dce4dc16a3e362c5ed88a0a9c873ae7
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2022-07-15 16:32:15 +0200

    add missing file

---
+++ lib/ui/homework.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeWork extends StatefulWidget {
+  const HomeWork({Key? key}) : super(key: key);
+
+  @override
+  State<HomeWork> createState() => _HomeWorkState();
+}
+
+class _HomeWorkState extends State<HomeWork> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.indigo,);
+  }
+}
