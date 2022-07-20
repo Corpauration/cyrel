2babb09de67f82e6b870f3b418f44745385639f1
alyrow <58048578+alyrow@users.noreply.github.com>
2022-07-20 18:08:32 +0200

    [linux] add icon in title bar

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -47,6 +47,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "cyrel");
   }
 
+  gtk_window_set_icon_from_file(window, "linux/icon.png", NULL);
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
