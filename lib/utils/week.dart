a950fb7fa3bbaa6213905a310c1a43f76db64257
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2022-08-13 14:28:30 +0200

    weekday name

--- lib/utils/week.dart
+++ lib/utils/week.dart
@@ -46,3 +46,18 @@ class Week {
     return "${formatDay(_begin)} - ${formatDay(_end)}";
   }
 }
+
+class WeekDay {
+  String name(int day) {
+    switch (day) {
+      case 0: return "Dimanche";
+      case 1: return "Lundi";
+      case 2: return "Mardi";
+      case 3: return "Mercredi";
+      case 4: return "Jeudi";
+      case 5: return "Vendredi";
+      case 6: return "Samedi";
+      default : return "";
+    }
+  }
+}
