af3a7cabfdad5d0a7da239564b39ef0664a233bd
alyrow <58048578+alyrow@users.noreply.github.com>
2022-08-18 10:32:27 +0200

    [linux] app name

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "cyrel");
+    gtk_header_bar_set_title(header_bar, "Cyrel");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "cyrel");
+    gtk_window_set_title(window, "Cyrel");
   }
 
   gtk_window_set_icon_from_file(window, "linux/icon.png", NULL);
