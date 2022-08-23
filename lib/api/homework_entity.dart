472c6b9eda6a1719580c3cf89ce719681608aae8
alyrow <58048578+alyrow@users.noreply.github.com>
2022-08-23 18:12:51 +0200

    remove homework level

--- lib/api/homework_entity.dart
+++ lib/api/homework_entity.dart
@@ -1,15 +1,12 @@
 import 'dart:core';
 
-enum HomeworkLevel { normal, info, warning, important }
-
 enum HomeworkType { dm, exo, ds }
 
 class HomeworkEntity {
   String title = "";
   String content = "";
   DateTime date = DateTime.now();
-  HomeworkLevel level = HomeworkLevel.normal;
   HomeworkType type = HomeworkType.exo;
 
-  HomeworkEntity(this.title, this.content, this.date, this.level, this.type);
+  HomeworkEntity(this.title, this.content, this.date, this.type);
 }
