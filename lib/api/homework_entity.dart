aa360ae6fbbc16bc65ccf1d781e00a0721fffcdf
alyrow <58048578+alyrow@users.noreply.github.com>
2022-08-23 18:34:39 +0200

    add homework entity fromJson constructor

--- lib/api/homework_entity.dart
+++ lib/api/homework_entity.dart
@@ -3,10 +3,18 @@ import 'dart:core';
 enum HomeworkType { dm, exo, ds }
 
 class HomeworkEntity {
+  String id = "";
   String title = "";
   String content = "";
   DateTime date = DateTime.now();
   HomeworkType type = HomeworkType.exo;
 
   HomeworkEntity(this.title, this.content, this.date, this.type);
+
+  HomeworkEntity.fromJson(Map<String, dynamic> json) :
+      id = json["id"],
+      title = json["title"],
+      content = json["content"],
+      date = DateTime.parse(json["date"]),
+      type = HomeworkType.values[json["type"]];
 }
