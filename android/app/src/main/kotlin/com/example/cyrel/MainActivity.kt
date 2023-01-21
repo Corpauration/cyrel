23fb3e1a6c3095b9beb74e928a8c83ec795436d0
alyrow <58048578+alyrow@users.noreply.github.com>
2023-01-21 23:03:40 +0100

    change package path

--- android/app/src/main/kotlin/com/example/cyrel/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package fr.corpauration.cyrel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/fr/corpauration/cyrel/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.corpauration.cyrel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
